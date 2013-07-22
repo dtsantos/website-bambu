6e34d7c280cccc4634bae9a83f2bd51f2732e246
Davi Trindade <davitrindade@gmail.com>
2013-07-22 18:10:26 -0300

    added google analytics script

--- casadebambuparaty/contatoen.py
+++ casadebambuparaty/contatoen.py
@@ -16,6 +16,19 @@ class MainPageEn(webapp2.RequestHandler):
 	<title>Bambu House Paraty - Contact Page</title>
 
 	<link rel="stylesheet" href="css/style.css" />
+    <script type="text/javascript">
+
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-38332351-1']);
+    _gaq.push(['_trackPageview']);
+
+    (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+    </script>
 </head>
 <body>
 	<div class="wrap">
@@ -123,7 +136,21 @@ class ConfirmacaoEn(webapp2.RequestHandler):
 
 	<title>Bambu House Paraty - Contact Page</title>
 	<link rel="stylesheet" href="css/style.css" />
+    <script type="text/javascript">
+
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-38332351-1']);
+    _gaq.push(['_trackPageview']);
+
+    (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+    </script>
 </head>
+
 <body>
 	<div class="wrap">
 		<header>
@@ -150,6 +177,7 @@ class ConfirmacaoEn(webapp2.RequestHandler):
 				</ul>
 			</div>
 		</header>
+        
 		
 		<div class="container-contato">
 			<h1>Message sent!</h1>
